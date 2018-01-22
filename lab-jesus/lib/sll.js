d2eb816bda8e42072187b91cdb9fd6f489df0938
sundownspecial <je.mendoza@live.com>
2018-01-22 08:45:47 -0800

    worked on reverse

--- lab-jesus/lib/sll.js
+++ lab-jesus/lib/sll.js
@@ -1,39 +1,65 @@
-const Nd = require('./nd')
-// import Nd from './nd'
-
+'use strict';
+const Nd = require('./nd.js');
 
 class SLL {
   constructor() {
     this.head = null;
   }
-
   insertHead(val) {
     let nd = new Nd(val);
-
     nd.next = this.head;
     this.head = nd;
     return this;
   }
-
   insertEnd(val) {
     let nd = new Nd(val);
-
     if(!this.head) {
       this.head = nd;
       return this;
     }
-
     for(var itr = this.head; itr.next; itr = itr.next);
     itr.next = nd;
     return this;
   }
-
-  remove() {
-
+  findMiddle() {
+    let counter = 0;
+    for(var itr = this.head; itr.next; itr = itr.next) {
+      counter++;
+    }
+    let middle = Math.floor(counter/2);
+    console.log(middle);
+    let newCounter = 0;
+    for(var itrNew = this.head; newCounter < middle; itrNew = itrNew.next) {
+      newCounter++;
+    }
+    console.log(itrNew.next);
+    return itrNew;
   }
+  reverse() {
+    let revArray = [];
+    for(var itr = this.head; itr.next; itr = itr.next) {
+      revArray.push(itr.value);
+    }
 
-  findNthNode() {
+    revArray.reverse();
+    console.log(revArray.reverse());
 
+    let count = revArray.length;
+
+    for(var itrRev = this.head; itrRev.next; itrRev = itrRev.next) {
+      this.value = revArray[count];
+      count--;
+    }
+
+
+
+    // let middle = Math.floor(counter/2);
+    // console.log(middle);
+    // let newCounter = 0;
+    // for(var itrNew = this.head; newCounter < middle; itrNew = itrNew.next) {
+    //   newCounter++;
+    // }
+    return this;
   }
 }
 
